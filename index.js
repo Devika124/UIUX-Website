741150927f3df1f941869e2591d490e7b7534630
Devika124 <99520363+Devika124@users.noreply.github.com>
2023-04-28 01:02:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+<!-- Hotjar Tracking Code for HappySmile -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3469738,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 let openMap = () => {
     window.location.assign("https://www.google.com/maps/place/Tahaj+Medical+Hall/@23.2188081,88.3500675,19.77z/data=!4m5!3m4!1s0x39f8e51a792abdc5:0xddc2842e1ef25816!8m2!3d23.2187196!4d88.3503924")
 }
